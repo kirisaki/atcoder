5f0241945de3d2ee244c4d3217ee811047ff4cfd
Akihito KIRISAKI <endouryusuke@realglobe.jp>
2019-03-14 11:55:25 +0900

    after/ABC121 D AC

--- abc121/abc121d.hs
+++ abc121/abc121d.hs
@@ -2,6 +2,10 @@ import Data.Bits
 
 main = do
   [a, b] <- map read . words <$> getLine :: IO [Int]
-  let xs = [a..b]
+  let xs = case (even a, even b) of
+        (True, True) -> [if even ((b - a) `div` 2) then 0 else 1, b]
+        (True, False) -> [if even ((b - a + 1) `div` 2) then 0 else 1]
+        (False, True) -> [a, if even ((b - a - 1) `div` 2) then 0 else 1, b]
+        (False, False) -> [a, if even ((b - a) `div` 2) then 0 else 1]
+
   putStrLn . show $ foldl1 xor xs
-  
