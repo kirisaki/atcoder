791247ad0b5bec0c7ae0a6ceb47b2c7833dcc0e0
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-03-20 20:37:01 +0900

    after / AGC031 A AC

--- agc031/agc031a.hs
+++ agc031/agc031a.hs
@@ -1,2 +1,2 @@
 import Data.List
-main = getLine >> getLine >>= putStrLn . show . (+ (-1)) . foldl1 (*) . map ((+) 1 . length) . group . sort
+main = getLine >> getLine >>= putStrLn . (show :: Integer -> String) . flip mod (10^9 + 7) . (+ (-1)) . foldl1 (*) . map ((+) 1 . fromIntegral . length) . group . sort
